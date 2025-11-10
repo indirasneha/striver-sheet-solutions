3cfa27a65ea8121ee3a46589d0a7482c4b4aea07
Indira Sneha <se23ucse133@mahindrauniversity.edu.in>
2025-11-11 00:12:15 +0530

    Day 2: Solved If Else statements Question

---
+++ 1.Basics/If_Else.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class If_Else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Marks= ");
+        int marks = sc.nextInt();
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 70) {
+            System.out.println("Grade B");
+        } else if (marks >= 50) {
+            System.out.println("Grade C");
+        } else if (marks >= 35) {
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Fail");
+        }
+        sc.close();
+    }
+}
